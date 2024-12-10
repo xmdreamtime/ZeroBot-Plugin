c98a47c707d7160a7057ccc34fc571de7665b43a
晓梦时光 <76523653+xmdreamtime@users.noreply.github.com>
2024-12-10 13:46:27 +0800

    Update sign_in.go

--- plugin/score/sign_in.go
+++ plugin/score/sign_in.go
@@ -29,7 +29,7 @@ import (
 )
 
 const (
-	backgroundURL = "https://t.alcy.cc/mp"
+	backgroundURL = "https://t.mwm.moe/pc"
 	referer       = "https://weibo.com/"
 	signinMax     = 1
 	// SCOREMAX 分数上限定为1200
